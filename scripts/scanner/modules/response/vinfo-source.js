468f001aed13cb438fecb646a2f1fae21bca23b7
Hugo Fortier <hugo@subgraph.com>
2010-10-15 17:22:18 -0400

    Regex fix for php source code detection

--- scripts/scanner/modules/response/vinfo-source.js
+++ scripts/scanner/modules/response/vinfo-source.js
@@ -4,20 +4,13 @@ var module = {
 };
 
 function run() {
-  var banner = response.header("Server");
-  var host = response.header("Host");
-
   var regexpasp = new Array(new RegExp("(<!--[^]*%[^]*?%(--)?>)","ig"), new RegExp("<%[^]*?%>","ig"));
-  var regexpphp = new Array(new RegExp("<\\?(?! *xml).*\\?>","ig"), new RegExp("<!--[^]*\\?[^]*?\\?(--)?>","ig"));
+  var regexpphp = new Array(new RegExp("<\\?(?! *xml)[^]*\\?>","ig"), new RegExp("<!--[^]*\\?[^]*?\\?(--)?>","ig"));
   var regexpjsp = new Array(new RegExp("<jsp:[^]*?>","gi"), new RegExp("<!--[^]*jsp:[^]?(--)?>","ig"));
   var i = 0;
   var output = "";
   var found = 0;
   var tmp = "";
-
-  if(!banner)
-	  return;
-
 
   for (i = 0; i < regexpasp.length; i++)
   {
